44feef7e2481520df3650fc93872dbdf394ef58e
hoacpa3101 <nhoa8012001@gmail.com>
2023-07-11 13:17:34 +0700

    no

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleQuestion, faCircleXmark, faEarthAsia, faKeyboard, faMoon, faSpinner } from '@fortawesome/free-solid-svg-icons';
-import Tippy from '@tippyjs/react/headless';
+import {
+    faCircleXmark,
+    faEarthAsia,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
+import { faCircleQuestion, faKeyboard, faMoon, faUser, } from '@fortawesome/free-regular-svg-icons';
+
+import HeadlessTippy from '@tippyjs/react/headless';
+import Tippy from '@tippyjs/react/';
+import 'tippy.js/dist/tippy.css'; 
 import classNames from 'classnames/bind';
 
 import { Wrapper as PopperWrapper } from '~/components/Popper';
@@ -15,31 +23,68 @@ const cx = classNames.bind(styles);
 const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
-        title: 'Tiếng Việt'
+        title: 'Tiếng Việt',
+        children: {
+            title: 'Ngôn ngữ',
+            data: [
+                {
+                    code: 'en',
+                    title: 'Tiếng Anh',
+                    type: 'language',
+                },
+                {
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                    type: 'language',
+                },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
         title: 'Phản hồi và trợ giúp',
-        to: './feedback'
+        to: './feedback',
     },
     {
         icon: <FontAwesomeIcon icon={faKeyboard} />,
-        title: 'Phím tắt trên bàn phím'
+        title: 'Phím tắt trên bàn phím',
     },
     {
         icon: <FontAwesomeIcon icon={faMoon} />,
-        title: 'Chế độ tối'
-    }
+        title: 'Chế độ tối',
+    },
+];
+
+const USER_MENU = [
+    {
+        icon: <FontAwesomeIcon icon={faUser} />,
+        title: 'Xem hồ sơ',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Nhận xu',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Cài đặt',
+    },
+    ...MENU_ITEMS
 ]
 
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
+    const currentUser = true;
 
     // Đây là phần để hiển thị kết quả tìm kiếm
-    // useEffect(() => {
-    //     setSearchResult([1, 2, 3]);
-    //     console.log(1);
-    // }, []);
+    useEffect(() => {
+        setSearchResult([1, 2, 3]);
+        console.log(1);
+    }, []);
+
+    // hangleLogic
+    const handleMenuChange = (menuItem) => {
+        console.log(menuItem);
+    };
 
     return (
         <header className={cx('wrapper')}>
@@ -82,7 +127,7 @@ function Header() {
                 </div>
 
                 {/* Đây là phân thông tin hiển thị ở ô search */}
-                <Tippy
+                <HeadlessTippy
                     visible={searchResult.length > 0}
                     interactive
                     render={(attrs) => (
@@ -121,38 +166,87 @@ function Header() {
                             </svg>
                         </button>
                     </div>
-                </Tippy>
-
-                
+                </HeadlessTippy>
                 <div className={cx('actions')}>
                     <Button text>Tải lên</Button>
-                    <Button primary>Đăng nhập</Button>
-                    {/* <Button outline large >Đăng nhập</Button> */}
-                    {/* <Button outline small>
-                        Follow
-                    </Button> */}
-                    {/* <Button rounded>Tải ứng dụng</Button> */}
+                    {currentUser ? (
+                        <>
+                            <Tippy content="Tin nhắn" placement='bottom'>
+                                <button className={cx('action-btn')}>
+                                    <svg
+                                        class="css-y48l9g-StyledIcon e1nx07zo1"
+                                        width="1em"
+                                        data-e2e=""
+                                        height="1em"
+                                        viewBox="0 0 48 48"
+                                        fill="currentColor"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            clipRule="evenodd"
+                                            d="M2.17877 7.17357C2.50304 6.45894 3.21528 6 4.00003 6H44C44.713 6 45.372 6.37952 45.7299 6.99615C46.0877 7.61278 46.0902 8.37327 45.7365 8.99228L25.7365 43.9923C25.3423 44.6821 24.5772 45.0732 23.7872 44.9886C22.9972 44.9041 22.3321 44.3599 22.0929 43.6023L16.219 25.0017L2.49488 9.31701C1.97811 8.72642 1.85449 7.88819 2.17877 7.17357ZM20.377 24.8856L24.531 38.0397L40.5537 10H8.40757L18.3918 21.4106L30.1002 14.2054C30.5705 13.9159 31.1865 14.0626 31.4759 14.533L32.5241 16.2363C32.8136 16.7066 32.6669 17.3226 32.1966 17.612L20.377 24.8856Z"
+                                        ></path>
+                                    </svg>
+                                </button>
+                            </Tippy>
+                            <Tippy content="Hộp thư" placement='bottom'>
+                                <button className={cx('action-btn')}>
+                                    <svg
+                                        class="css-1g0p6jv-StyledInboxIcon e18kkhh41"
+                                        width="32"
+                                        data-e2e=""
+                                        height="32"
+                                        viewBox="0 0 32 32"
+                                        fill="currentColor"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            clipRule="evenodd"
+                                            d="M24.0362 21.3333H18.5243L15.9983 24.4208L13.4721 21.3333H7.96047L7.99557 8H24.0009L24.0362 21.3333ZM24.3705 23.3333H19.4721L17.2883 26.0026C16.6215 26.8176 15.3753 26.8176 14.7084 26.0026L12.5243 23.3333H7.62626C6.70407 23.3333 5.95717 22.5845 5.9596 21.6623L5.99646 7.66228C5.99887 6.74352 6.74435 6 7.66312 6H24.3333C25.2521 6 25.9975 6.7435 26 7.66224L26.0371 21.6622C26.0396 22.5844 25.2927 23.3333 24.3705 23.3333ZM12.6647 14C12.2965 14 11.998 14.2985 11.998 14.6667V15.3333C11.998 15.7015 12.2965 16 12.6647 16H19.3313C19.6995 16 19.998 15.7015 19.998 15.3333V14.6667C19.998 14.2985 19.6995 14 19.3313 14H12.6647Z"
+                                        ></path>
+                                    </svg>
+                                </button>
+                            </Tippy>
+                        </>
+                    ) : (
+                        <>
+                            <Button primary>Đăng nhập</Button>
+                            {/* <Button outline large >Đăng nhập</Button> */}
+                            {/* <Button outline small>
+                             Follow
+                            </Button> */}
+                            {/* <Button rounded>Tải ứng dụng</Button> */}
 
-                    {/* Đây là phần hiển thị thông tin ở dấu 3 chấm cạnh đăng nhập */}
-                    <Menu
-                        items ={MENU_ITEMS}
-                    >
-                        <div className={cx('more-btn')}>
-                            <svg
-                                width="1em"
-                                data-e2e=""
-                                height="1em"
-                                viewBox="0 0 48 48"
-                                fill="currentColor"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M24 4C26.2091 4 28 5.79086 28 8C28 10.2091 26.2091 12 24 12C21.7909 12 20 10.2091 20 8C20 5.79086 21.7909 4 24 4ZM24 20C26.2091 20 28 21.7909 28 24C28 26.2091 26.2091 28 24 28C21.7909 28 20 26.2091 20 24C20 21.7909 21.7909 20 24 20ZM24 36C26.2091 36 28 37.7909 28 40C28 42.2091 26.2091 44 24 44C21.7909 44 20 42.2091 20 40C20 37.7909 21.7909 36 24 36Z"
-                                ></path>
-                            </svg>
-                        </div>
+                            {/* Đây là phần hiển thị thông tin ở dấu 3 chấm cạnh đăng nhập */}
+                        </>
+                    )}
+
+                    <Menu items={currentUser ? USER_MENU : MENU_ITEMS} onChange={handleMenuChange}>
+                        {currentUser ? (
+                            <img
+                                className={cx('user-avatar')}
+                                src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/0084894d9c5e92339f6346def4819ddc~c5_100x100.jpeg?x-expires=1689217200&x-signature=tuTasFoq5sWvZqB9pC8SWYERE0o%3D"
+                            />
+                        ) : (
+                            <div className={cx('more-btn')}>
+                                <svg
+                                    width="1em"
+                                    data-e2e=""
+                                    height="1em"
+                                    viewBox="0 0 48 48"
+                                    fill="currentColor"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        clipRule="evenodd"
+                                        d="M24 4C26.2091 4 28 5.79086 28 8C28 10.2091 26.2091 12 24 12C21.7909 12 20 10.2091 20 8C20 5.79086 21.7909 4 24 4ZM24 20C26.2091 20 28 21.7909 28 24C28 26.2091 26.2091 28 24 28C21.7909 28 20 26.2091 20 24C20 21.7909 21.7909 20 24 20ZM24 36C26.2091 36 28 37.7909 28 40C28 42.2091 26.2091 44 24 44C21.7909 44 20 42.2091 20 40C20 37.7909 21.7909 36 24 36Z"
+                                    ></path>
+                                </svg>
+                            </div>
+                        )}
                     </Menu>
                 </div>
             </div>
